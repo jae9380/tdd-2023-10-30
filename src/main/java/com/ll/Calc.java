bbbef4f4bc0e740192e3d1632012578de06d26bc
jae9380 <ljy5314@gmail.com>
2023-10-30 11:17:10 +0900

    red단계

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,10 +1,24 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calc {
 
     public static int run1(String exp){
-        return 20 ;
+        int reslut=0;
+
+        return reslut;
     }
 
 
+//    public static int run2(String exp) {
+//        return (a*1+(1-1));
+//    }
+//
+//    public static int run3(String exp) {
+//        return (a*1+(1-((a+1)*1-(1+1))));
+//    }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -3,10 +3,17 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalcTest {
     @Test
     @DisplayName("Calc에 runMethod는 int를 return 한다")
     void t1(){
         int answer = Calc.run1("10+10");
+        assertThat(answer).isEqualTo(20);
+    }
+    void t2(){
+        int answer = Calc.run1("10-10");
+        assertThat(answer).isEqualTo(0);
     }
 }
